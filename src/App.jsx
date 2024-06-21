0d49eae2a35a7f77784782bd60d571fda75cc205
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-21 12:54:57 +0200

    adding global styles with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,27 @@
 import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
-
-const StyledApp = styled.div`
+const StyledApp = styled.main`
   background-color: orangered;
   padding: 20px;
 `;
 
 const App = () => {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button>Check-in</Button>
-      <Button>Check-out</Button>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading>The Wild Oasis</Heading>
+        <Button>Check-in</Button>
+        <Button>Check-out</Button>
 
-      <Input type="number" placeholder="Number of guests" />
-      <Input type="number" placeholder="Number of guests" />
-    </StyledApp>
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
   );
 };
 
