e17c5af36d4437b4fe946ffbc00e9db667553907
Kanan Farzali <kanan.farzali@gmail.com>
2024-06-23 14:18:17 +0200

    displaying toasts with react hot toast package

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { formatCurrency } from '../../utils/helpers';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteCabin } from '../../services/apiCabins';
+import toast from 'react-hot-toast';
 
 const TableRow = styled.div`
   display: grid;
@@ -57,14 +58,14 @@ const CabinRow = ({ cabin }) => {
   const { isPending, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert('Cabin was successfully deleted');
+      toast.success('Cabin was successfully deleted');
 
       queryClient.invalidateQueries({
         queryKey: ['cabins'],
       });
     },
     onError: (err) => {
-      alert(err.message);
+      toast.error(err.message);
     },
   });
 
