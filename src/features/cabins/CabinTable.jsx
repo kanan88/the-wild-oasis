6a28c784bd6e7187cb0b55bde64c2f7eff389cb8
Kanan Farzali <kanan.farzali@gmail.com>
2024-07-21 22:43:03 +0200

    applying the render props pattern to reusable table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -36,9 +36,10 @@ const CabinTable = () => {
         <div></div>
       </Table.Header>
 
-      {cabins.map((cabin) => (
-        <CabinRow key={cabin.id} cabin={cabin} />
-      ))}
+      <Table.Body
+        data={cabins}
+        render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
+      />
     </Table>
   );
 };
