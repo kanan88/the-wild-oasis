22b6fb645f08a6d136706053bfc2d6c656877975
Kanan Farzali <kanan.farzali@gmail.com>
2024-07-22 23:11:12 +0200

    building a resuable pagination component

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from '../../ui/Menus';
 import Empty from '../../ui/Empty';
 import { useBookings } from './useBookings';
 import Spinner from '../../ui/Spinner';
+import Pagination from '../../ui/Pagination';
 
 const BookingTable = () => {
   const { isPending, bookings } = useBookings();
@@ -30,6 +31,10 @@ const BookingTable = () => {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+
+        <Table.Footer>
+          <Pagination count={45} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -50,7 +50,7 @@ const Filter = ({ filterField, options }) => {
         <FilterButton
           key={option.value}
           onClick={() => handleClick(option.value)}
-          active={option.value === currentFilter}
+          active={option.value === currentFilter ? 1 : 0}
           disabled={option.value === currentFilter}
         >
           {option.label}
