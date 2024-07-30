7985b07a9002454096382e72b4d27b425969479e
Kanan Farzali <kanan.farzali@gmail.com>
2024-07-30 11:26:26 +0200

    checking out a booking

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -12,6 +12,8 @@ import { useMoveBack } from '../../hooks/useMoveBack';
 import { useBooking } from './useBooking';
 import Spinner from '../../ui/Spinner';
 import { useNavigate } from 'react-router-dom';
+import { HiArrowUpOnSquare } from 'react-icons/hi2';
+import { useCheckout } from '../check-in-out/useCheckout';
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -22,6 +24,7 @@ const HeadingGroup = styled.div`
 const BookingDetail = () => {
   const navigate = useNavigate();
   const { booking, isPending } = useBooking();
+  const { checkout, isCheckingOut } = useCheckout();
 
   const moveBack = useMoveBack();
 
@@ -48,12 +51,24 @@ const BookingDetail = () => {
       <BookingDataBox booking={booking} />
 
       <ButtonGroup>
-        <Button
-          variation="secondary"
-          onClick={() => navigate(`/checkin/${bookingId}`)}
-        >
-          Check-in
-        </Button>
+        {status === 'unconfirmed' && (
+          <Button
+            variation="secondary"
+            onClick={() => navigate(`/checkin/${bookingId}`)}
+          >
+            Check-in
+          </Button>
+        )}
+
+        {status === 'checked-in' && (
+          <Button
+            icon={<HiArrowUpOnSquare />}
+            onClick={() => checkout(bookingId)}
+            disabled={isCheckingOut}
+          >
+            Check out
+          </Button>
+        )}
 
         <Button variation="secondary" onClick={moveBack}>
           Back
